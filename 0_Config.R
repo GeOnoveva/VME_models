b36f3a0c49a4b60c5f23f541690d023e3b63e1fe
Gonzalez-Mirelis <genoveva@hi.no>
2023-07-25 10:10:34 +0200

    add code for function to derive buffer distances

--- 0_Config.R
+++ 0_Config.R
@@ -130,3 +130,28 @@ threspol <- function(thresvalue, raster, min.area, max.hole){
   pol_dens <- pol
   return(pol)
 }
+
+## function to derive buffer distances for a set of points (adapted from T Hengl)
+
+setMethod("buffer.dist", signature(observations = "SpatialPointsDataFrame", predictionDomain = "SpatialPixelsDataFrame"), function(observations, predictionDomain, classes, width, ...){
+  if(missing(width)){ width <- sqrt(areaSpatialGrid(predictionDomain)) }
+  if(!length(classes)==length(observations)){ stop("Length of 'observations' and 'classes' does not match.") }
+  ## remove classes without any points:
+  xg = summary(classes, maxsum=length(levels(classes)))
+  selg.levs = attr(xg, "names")[xg > 0]
+  if(length(selg.levs)<length(levels(classes))){
+    fclasses <- as.factor(classes)
+    fclasses[which(!fclasses %in% selg.levs)] <- NA
+    classes <- droplevels(fclasses)
+  }
+  ## derive buffer distances
+  s <- list(NULL)
+  for(i in 1:length(levels(classes))){
+    s[[i]] <- raster::distance(rasterize(observations[which(classes==levels(classes)[i]),1]@coords, y=raster(predictionDomain)), width=width, ...)
+  }
+  s <- s[sapply(s, function(x){!is.null(x)})]
+  s <- brick(s)
+  s <- as(s, "SpatialPixelsDataFrame")
+  s <- s[predictionDomain@grid.index,]
+  return(s)
+})
