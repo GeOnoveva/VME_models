531abf3dab841f912ddb5d8fea114dd87811c782
GeOnoveva <selfishgeno@gmail.com>
2023-07-27 15:00:24 +0200

    data input updated

--- fake_marvid_report_historical_data.R
+++ fake_marvid_report_historical_data.R
@@ -1,23 +1,17 @@
 
 library(dplyr)
 
-old_data <- read.csv("U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\Data_Delivery_2024\\species_densities.csv")
+data <- read.csv(file.path(Data,"species_densities.csv"))
 
-old_data <- old_data %>% rename(taxon=clean_taxonomy, density = density_n100m2) %>% select(SampID, taxon, density)
-
-data <- old_data
+data <- data %>% rename(taxon=clean_taxonomy, density = density_n100m2) %>% select(SampID, taxon, density)
 
 
-old_sample_info <- read.csv("U:\\Mareano\\VIDEOLAB\\VIDEO DATA\\200m_scale_species_by_sample\\Data_Delivery_2023\\inputs\\sample_info.csv")
 
-old_sample_info <- old_sample_info %>% rename(video.line=VL, section=SampID2, mean.latitude=y_coord, mean.longitude=x_coord) %>%
+sample_info <- read.csv(file.path(Data,"sample_info.csv"))
+
+sample_info <- sample_info %>% rename(video.line=VL, section=SampID2, mean.latitude=y_coord, mean.longitude=x_coord) %>%
   select(video.line, section, mean.latitude, mean.longitude, SampID)
 
-sample_info <- old_sample_info
-
-# IMPORTANT NOTE: I may have to re-generate sample_info and species_densities (from the make matrix generalized notebook!!)
-
-rm(old_data, old_sample_info)
 
 
 ## fake spatial pixels data frame (rather, incomplete)
