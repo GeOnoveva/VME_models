7abbbf5b74c163303d376b4d5258fc5c677e4eb5
Gonzalez-Mirelis <genoveva@hi.no>
2023-10-02 13:13:09 +0200

    remove the part about spatial pixels

--- fake_marvid_report_historical_data.R
+++ fake_marvid_report_historical_data.R
@@ -11,35 +11,3 @@ sample_info <- read.csv(file.path(Data,"sample_info.csv"))
 
 sample_info <- sample_info %>% rename(video.line=VL, section=SampID2, mean.latitude=y_coord, mean.longitude=x_coord) %>%
   select(video.line, section, mean.latitude, mean.longitude, SampID)
-
-
-
-## fake spatial pixels data frame (rather, incomplete)
-
-i<-1
-
-predpix <- raster(paste0(ftp_filepath,
-                         paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""))
-)
-names(predpix) <- substr(paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""),
-                         2,
-                         nchar(paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""))-4)
-predpix <- aggregate(predpix,4)
-
-predpix <- as(predpix, "SpatialPixelsDataFrame")
-
-
-i<-2
-
-tmpraster <- raster(paste0(ftp_filepath,
-                           paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""))
-)
-
-tmpraster <- aggregate(tmpraster,4)
-
-
-predpix[[i]]<- values(tmpraster)[predpix@grid.index]
-
-names(predpix@data)[[2]]<-substr(paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""),
-                                 2,
-                                 nchar(paste(unlist(strsplit(files[i], split="all", fixed=TRUE))[2], sep=""))-4)
