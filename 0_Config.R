c9fda908e57fc69ff889a4543e27267bec047697
Gonzalez-Mirelis <genoveva@hi.no>
2023-11-14 11:09:36 +0100

    ad lines to create data and sample info, which need to be there from the start

--- 0_Config.R
+++ 0_Config.R
@@ -166,3 +166,13 @@ setMethod("buffer.dist", signature(observations = "SpatialPointsDataFrame", pred
   s <- s[predictionDomain@grid.index,]
   return(s)
 })
+
+## get data while not on MarVid
+library(dplyr)
+data <- read.csv(file.path(Data,"species_densities.csv"))
+data <- data %>% rename(taxon=clean_taxonomy, density = density_n100m2) %>% dplyr::select(SampID, taxon, density)
+
+sample_info <- read.csv(file.path(Data,"sample_info.csv"))
+sample_info <- sample_info %>% rename(video.line=VL, section=SampID2, mean.latitude=y_coord, mean.longitude=x_coord) %>%
+  dplyr::select(video.line, section, mean.latitude, mean.longitude, SampID)
+
